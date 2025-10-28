c35260ee98c61d6a6a5dd667fb1e90cf4038674b
MunezeroEugene <0788647843ab@gmail.com>
2025-10-28 14:30:00 +0200

    project setup

---
+++ src/main/java/com/mun/theatrems/TheatremsApplication.java
@@ -0,0 +1,13 @@
+package com.mun.theatrems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheatremsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheatremsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mun/theatrems/TheatremsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mun.theatrems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheatremsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
